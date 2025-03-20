efa0252c1327210d304ff990eb94374b2b4b63a1
locnpIT <npl2004qn@gmail.com>
2025-03-21 02:57:10 +0700

    upload

---
+++ models/event.go
@@ -0,0 +1,23 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID          int
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"` //chỗ này nếu mình không điền thì báo lỗi
+	UserID      int
+}
+
+var events []Event = []Event{}
+
+func (e Event) Save() {
+	// later: add ot tp a database
+	events = append(events)
+}
+
+func GetAllEvents() []Event {
+	return events
+}
