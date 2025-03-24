1a6835e6d806223bd9bd03904a37f298b4c15e61
locnpIT <npl2004qn@gmail.com>
2025-03-24 23:53:36 +0700

    upload

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"loc.com/hocgolang/db"
+	"loc.com/hocgolang/routes"
 )
 
 func main() {
@@ -10,6 +11,8 @@ func main() {
 	db.InitDB()
 
 	server := gin.Default()
+
+	routes.RegisterRoutes(server)
 
 	server.Run(":8080") //localhost:8080
 
