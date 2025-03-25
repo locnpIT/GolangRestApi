ae786c82c6e12c94ed248486a91caee9cfba2b5d
locnpIT <npl2004qn@gmail.com>
2025-03-26 02:42:31 +0700

    upload hash password

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "loc.com/hocgolang/db"
+import (
+	"loc.com/hocgolang/db"
+	"loc.com/hocgolang/utils"
+)
 
 type User struct {
 	ID       int64
@@ -18,7 +21,12 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+	result, err := stmt.Exec(u.Email, hashedPassword)
 
 	if err != nil {
 		return err
