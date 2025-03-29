ea6b1869298e4dcb16aed8926ace30f2e4d9f380
locnpIT <npl2004qn@gmail.com>
2025-03-29 23:46:58 +0700

    upload

--- models/user.go
+++ models/user.go
@@ -37,5 +37,34 @@ func (u User) Save() error {
 	u.ID = userId
 
 	return err
+	// upload code to github
+}
 
+func (u User) save() error {
+	query := `INSERT INTO users(email, password) VALUES(?,?)`
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	hashedPassword, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+	result, err := stmt.Exec(u.Email, hashedPassword)
+
+	if err != nil {
+		return err
+	}
+
+	userId, err := result.LastInsertId()
+
+	u.ID = userId
+
+	return err
+	// upload code to github
 }
